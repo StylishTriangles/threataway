6e1dc443d989d93d7f8b8851efb0f7ec00c6749f
Michal Pogoda <michalpogoda@hotmail.com>
2018-11-24 19:55:20 +0000

    Drobnostki

--- source/controller/lists.go
+++ source/controller/lists.go
@@ -2,11 +2,8 @@ package controller
 
 import (
 	"gowebapp/source/model/lists"
-	"gowebapp/source/model/user"
 	"gowebapp/source/view"
-	"log"
 	"net/http"
-	"strconv"
 )
 
 func listsHandler(w http.ResponseWriter, r *http.Request) {
@@ -19,40 +16,4 @@ func listsHandler(w http.ResponseWriter, r *http.Request) {
 
 	v.Vars["Lists"] = lists
 	v.Render(w)
-}
-
-func listsDeleteHandler(w http.ResponseWriter, r *http.Request) {
-	// Check if user has write permissions
-	session, err := store.Get(r, "session")
-	auth, ok := session.Values["authenticated"]
-	if err != nil || !ok || !auth.(bool) {
-		http.Error(w, "Please log in to view this page", 403)
-		return
-	}
-	user, ok := session.Values["user"].(*user.User)
-	if !ok {
-		log.Println("Corrupt cookie data")
-		http.Error(w, "An error occoured: corrupt cookie data", 500)
-		return
-	}
-	if user.Role < 1 {
-		http.Error(w, "Insufficient permisions (you need write access to perform this action", 403)
-	}
-
-	// Delete lists with specified IDs provided via json from frontend
-	r.ParseForm()
-	ids, ok := r.Form["ids"]
-	if !ok {
-		http.Error(w, "Invalid request", 500)
-	}
-	var parsedIDs []uint32
-	for _, sid := range ids {
-		val, _ := strconv.Atoi(sid)
-		parsedIDs = append(parsedIDs, uint32(val))
-	}
-
-	err = lists.DeleteLists(parsedIDs, user.ID)
-	if err != nil {
-		log.Println(err.Error())
-	}
 }
