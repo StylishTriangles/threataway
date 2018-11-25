296831588c2179de5b545e009ce54978475493dc
Michal Pogoda <michalpogoda@hotmail.com>
2018-11-25 02:36:32 +0000

    wyswietlanie list

--- source/model/domain/domain.go
+++ source/model/domain/domain.go
@@ -43,6 +43,37 @@ func GetAll() ([]Domain, error) {
 	return ret, nil
 }
 
+func GetFromList(listName string) ([]Domain, error) {
+	var ret []Domain
+	tx, err := database.DB.Begin()
+	if err != nil {
+		return nil, err
+	}
+	defer tx.Rollback()
+
+	// Check if username already exists in db
+	stmt, err := tx.Prepare("SELECT idUrl, domain, rating FROM urls")
+	if err != nil {
+		return nil, err
+	}
+	defer stmt.Close()
+	rows, err := stmt.Query()
+	if err != nil {
+		return nil, err
+	}
+	for rows.Next() {
+		d := Domain{}
+		err = rows.Scan(&d.ID, &d.URL, &d.Rating)
+		if err != nil {
+			return nil, err
+		}
+		ret = append(ret, d)
+	}
+
+	tx.Commit()
+	return ret, nil
+}
+
 // RegisterNew creates a new database record for given domain
 func RegisterNew(domain string) error {
 	stmt, err := database.DB.Prepare("INSERT INTO urls(domain) VALUES(?)")
