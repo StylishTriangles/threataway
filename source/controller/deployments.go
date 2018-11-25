c22fd6c0804875fc5bb8deac534e4191bc0b1102
StylishTriangles <hungrycow2012@gmail.com>
2018-11-25 04:53:41 +0100

    delete deployments

--- source/controller/deployments.go
+++ source/controller/deployments.go
@@ -83,3 +83,48 @@ func deploymentsAdd(w http.ResponseWriter, r *http.Request) {
 	}
 	w.Write([]byte(fmt.Sprintf("Success publishing new list, it will be available at /l/%s", url)))
 }
+
+func deploymentsDeleteHandler(w http.ResponseWriter, r *http.Request) {
+	// Check if user has write permissions
+	session, err := store.Get(r, "session")
+	auth, ok := session.Values["authenticated"]
+	if err != nil || !ok || !auth.(bool) {
+		http.Error(w, "Please log in to view this page", 403)
+		log.Println("Please log in to view this page")
+		return
+	}
+	user, ok := session.Values["user"].(*user.User)
+	if !ok {
+		log.Println("Corrupt cookie data")
+		http.Error(w, "An error occoured: corrupt cookie data", 500)
+		return
+	}
+	if user.Role < 1 {
+		log.Println("Insufficient permisions (you need write access to perform this action)")
+		http.Error(w, "Insufficient permisions (you need write access to perform this action)", 403)
+		return
+	}
+
+	// Delete lists with specified IDs provided via json from frontend
+	r.ParseForm()
+	ids, ok := r.Form["ids[]"]
+	if !ok {
+		http.Error(w, "Invalid request", 400)
+		log.Println("Invalid request")
+		log.Println(r.Form)
+		return
+	}
+	var parsedIDs []uint32
+	for _, sid := range ids {
+		val, _ := strconv.Atoi(sid)
+		parsedIDs = append(parsedIDs, uint32(val))
+	}
+
+	err = deployments.DeleteDeployments(parsedIDs)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, err.Error(), 500)
+		return
+	}
+	w.Write([]byte("Delete success"))
+}
